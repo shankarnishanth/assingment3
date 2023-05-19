2d629d9f80c7b59acf2f5ac97843b77bf81b0888
shankarnishanth <nishanth23shankar@gmail.com>
2023-05-19 16:11:19 +0530

    added:java_reverse

---
+++ src/reverse.java
@@ -0,0 +1,14 @@
+public class reverse {
+    public static void main(String[] args) {
+        int array[]=new int[6];
+        array[5] = 1;
+        array[4] = 2;
+        array[3] = 3;
+        array[2] = 4;
+        array[1] = 5;
+        array[0] = 6;
+        for(int i = 0; i < array .length;i++){
+            System.out.println(array[i]);
+
+    }}}
+
