c3ca8ae4533b59193e43af555ec74a6315326022
shankarnishanth <nishanth23shankar@gmail.com>
2023-05-19 14:34:53 +0530

    added:java_element

---
+++ src/elements.java
@@ -0,0 +1,13 @@
+public class elements {
+    public static void main(String[] args) {
+        int array[]=new int[6];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        array[3] = 4;
+        array[4] = 5;
+        array[5] = 6;
+        for(int i = 0; i < array .length;i++){
+            System.out.println(array[i]);
+    }
+}}
